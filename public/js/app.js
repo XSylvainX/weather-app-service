e077ef421e85bb3aead0072f1f537d2ad946bdd0
XSylvainX <37834208+XSylvainX@users.noreply.github.com>
2021-07-29 19:40:15 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@
 console.log('client side javascrip is loading')
 
 
-//select the part of the document who I want interact
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -12,14 +11,14 @@ const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    //take the input value
+  
     const location= search.value
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent= ''
 
-    // add the input in the request to find informatons 
-    //fetch('http://localhost:3000/weather?address='+ location)
+   
+   
     fetch('/weather?address='+ location)
     .then((response) => {
         response.json()
